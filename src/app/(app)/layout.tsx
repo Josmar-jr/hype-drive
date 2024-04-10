eef1b9193050dc0f3b4e141114fd7998351e7532
Josmar Soares Trigueiro Junior <josmar.junior@cubos.io>
2024-04-10 16:04:04 -0300

    feat: initial template and authentication integration 🐙

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+import { Header } from "@/components/header";
+import { Toaster } from "@/components/ui/toaster";
+import { QueryClientProvider } from "@/providers/QueryClientProvider";
+
+export default function AppLayout({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider>
+      <div className="bg-background min-h-screen">
+        <Header />
+
+        <main className="p-6 max-w-7xl mx-auto">{children}</main>
+
+        <Toaster />
+      </div>
+    </QueryClientProvider>
+  );
+}
